48035297f0ce8e7fc061d7a777b9433070c079ec
Brent Perry <burentoperii@gmail.com>
2016-12-19 08:52:14 -0600

    build initial array, and remove multiples of index 0.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,42 @@
-$(document).ready(function() {
-  $("form#word-submit").submit(function(event) {
-    event.preventDefault();
-    var input = $("#word").val();
-    $("#input-word").text(input);
-    if (!isPalindrome(input)) {
-      $("#not").show();
+var allNums = [];
+var primes = [];
+
+var isPrime = function(number) {
+  if (number < 2) {
+    return false;
+  } else {
+    for (var index = 2; index < number; index += 1) {
+      if (number % index === 0) {
+        return false;
+      }
     }
+    return true;
+  }
+}
+
+var removeMultiples = function(array) {
+  for (var index = 1; index < array.length; index += 1) {
+    if (array[index] % array[0] === 0) {
+      array.splice(index, 1);
+    }
+  }
+}
+
+var getPrimes = function(number) {
+  for (var index = 2; index < number; index += 1) {
+    allNums.push(index);
+  }
+
+}
+
+$(document).ready(function() {
+  $("form#number-submit").submit(function(event) {
+    event.preventDefault();
+    var input = parseInt($("#number").val());
+    $("#input").text(input);
+    getPrimes(input);
+    removeMultiples(allNums);
+    console.log(allNums);
     $(".output").show();
   })
 })
